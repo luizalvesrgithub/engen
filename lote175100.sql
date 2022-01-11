cf1391a29eaed537e4de9de2d066f20e5728ec23
luizalvesrgithub <luizalvesr@gmail.com>
2022-01-11 14:40:42 -0300

    Add files via upload

---
+++ lote175100.sql
@@ -0,0 +1,50 @@
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146608 AND DIGITO = 'YEQD' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 147493 AND DIGITO = 'WBYE' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146613 AND DIGITO = 'VUVT' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146600 AND DIGITO = 'WHIE' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146603 AND DIGITO = 'ARFJ' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146644 AND DIGITO = 'JFGO' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146619 AND DIGITO = 'BHMU' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146906 AND DIGITO = 'IFPH' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146609 AND DIGITO = 'RHOA' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146610 AND DIGITO = 'TJTG' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146532 AND DIGITO = 'QYEI' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146623 AND DIGITO = 'VEVW' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146535 AND DIGITO = 'SSFB' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146602 AND DIGITO = 'TTJM' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146571 AND DIGITO = 'YMPK' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 147562 AND DIGITO = 'HVOI' AND Status = 'C' ; <====
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 147248 AND DIGITO = 'BKBO' AND Status = 'C' ; <====
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 130428 AND DIGITO = 'ZHRE' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 130429 AND DIGITO = 'ODIV' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146520 AND DIGITO = 'TBJT' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146519 AND DIGITO = 'WGTH' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146525 AND DIGITO = 'YFBE' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146500 AND DIGITO = 'YXJA' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146499 AND DIGITO = 'AGDO' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146498 AND DIGITO = 'BCEO' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146517 AND DIGITO = 'BRFI' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146497 AND DIGITO = 'WCEF' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146516 AND DIGITO = 'OMDL' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146598 AND DIGITO = 'LDVE' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146511 AND DIGITO = 'XMHM' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146492 AND DIGITO = 'QIZH' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146493 AND DIGITO = 'LNOF' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146566 AND DIGITO = 'DBFZ' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146507 AND DIGITO = 'GHOJ' AND Status = 'C' ;  <====
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 147561 AND DIGITO = 'JLOS' AND Status = 'C' ;  <==== 
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146495 AND DIGITO = 'RSDE' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146487 AND DIGITO = 'RWMD' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 124360 AND DIGITO = 'ZNHO' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 147018 AND DIGITO = 'IKGE' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 147002 AND DIGITO = 'ENLQ' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 147003 AND DIGITO = 'IEQM' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 147054 AND DIGITO = 'KHMT' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146945 AND DIGITO = 'ECKO' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 147067 AND DIGITO = 'UMFB' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146943 AND DIGITO = 'TZRX' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146942 AND DIGITO = 'LBLC' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146896 AND DIGITO = 'CCMY' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 146524 AND DIGITO = 'FRTT' AND Status = 'C' ;
+update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = 2021 AND CartorioExtrajudicial = 22 AND SEQUENCIAL = 108015 AND DIGITO = 'PIPY' AND Status = 'C' ;
+
